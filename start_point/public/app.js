02234e0d1febda74e4488f47dab162cacf839f27
Cyrus <cdbalsara@icloud.com>
2016-12-06 07:56:36 +0000

    included cat name and favorite food in description

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -1,42 +1,46 @@
 
-// // var cats = document.getElementsByClass("cat");
-// // console.log(cats)
+// var cats = document.getElementsByClass("cat");
+// console.log(cats)
 
 
-// // Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
+// Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
 
-// window.onload = function(){
+window.onload = function(){
 
-// // which section will you be working in?
-// var cats = document.getElementById('cats');
+// which section will you be working in?
+var cats = document.getElementById('cats');
 
-// // where are you actually adding the new information?
-// var catClass = document.createElement('ul');
-// catClass.classList.add("cat");
+// where are you actually adding the new information?
+var catClass = document.createElement('ul');
+catClass.classList.add("cat");
 
-// // what new information will you be adding? 
-// var li1 = document.createElement("li");
-// li1.innerText = "Name: Garfield" ;
+// what new information will you be adding? 
+var li1 = document.createElement("li");
+li1.innerText = "Name: Garfield" ;
 
-// var li2 = document.createElement("li");
-// li2.innerText = "Favourite food: Lasagne";
+var li2 = document.createElement("li");
+li2.innerText = "Favourite food: Lasagne";
 
-// var img = document.createElement("img");
-// img.src = "http://comicsalliance.com/files/2015/06/garfield01.jpg"
+var img = document.createElement("img");
+img.src = "http://comicsalliance.com/files/2015/06/garfield01.jpg"
 
-// // need to link up all the var to their parent now
-// // (use the indentation in the index.html as the hint to see the family tree)
-// catClass.appendChild(li1);
-// catClass.appendChild(li2);
-// catClass.appendChild(img);
-// cats.appendChild(catClass);
-// };
+// need to link up all the var to their parent now
+// (use the indentation in the index.html as the hint to see the family tree)
+catClass.appendChild(li1);
+catClass.appendChild(li2);
+catClass.appendChild(img);
+cats.appendChild(catClass);
+};
+
+
+
+
 
 // Refactor the code to make it easy to add a new cat, just like we did with the quotes.
 
 window.onload = function(){
 
-var catClass = createUl("Garfield", "Lasagne","http://comicsalliance.com/files/2015/06/garfield01.jpg");
+var catClass = createUl("Name: Garfield", "Favourite food: Lasagne","http://comicsalliance.com/files/2015/06/garfield01.jpg");
 };
 
 
